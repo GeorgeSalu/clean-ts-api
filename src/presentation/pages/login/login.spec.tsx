2d50e433d0b59b7c64cdc6a4fa39c3609bbc444e
George Salu <george.salu10@gmail.com>
2021-02-08 17:24:16 -0300

    test: garante que Login apresenta erro se email invalido

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -11,6 +11,8 @@ type SutTypes = {
 
 const makeSut = (): SutTypes => {
   const valitationSpy = new ValidationSpy()
+
+  valitationSpy.errorMessage = faker.random.words()
   const sut = render(<Login validation={valitationSpy} />)
   return {
     sut,
@@ -22,13 +24,13 @@ describe('Login component', () => {
   afterEach(cleanup)
 
   test('should start with initial state', () => {
-    const { sut } = makeSut()
+    const { sut, valitationSpy } = makeSut()
     const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
     const emailStatus = sut.getByTestId('email-status')
-    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.title).toBe(valitationSpy.errorMessage)
     expect(emailStatus.textContent).toBe('🔴')
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe('Campo obrigatório')
@@ -51,5 +53,15 @@ describe('Login component', () => {
     fireEvent.input(passwordInput, { target: { value: password } })
     expect(valitationSpy.fieldName).toBe('password')
     expect(valitationSpy.fieldValue).toBe(password)
+  })
+
+  test('shoould show email error if validation fails', () => {
+    const { sut, valitationSpy } = makeSut()
+
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe(valitationSpy.errorMessage)
+    expect(emailStatus.textContent).toBe('🔴')
   })
 })
