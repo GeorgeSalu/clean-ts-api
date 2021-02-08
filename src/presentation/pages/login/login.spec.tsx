974da93e62dfc85cd63126eee13770ef5d23fe4a
George Salu <george.salu10@gmail.com>
2021-02-08 09:17:54 -0300

    fix: correção texto

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import Login from './login'
 
 describe('Login component', () => {
-  test('should not render spinner and erro on start', () => {
+  test('should not render spinner and error on start', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
