f01735fe974c9d8bfcbda9732805be4dda3e15f9
George Salu <george.salu10@gmail.com>
2021-02-08 21:55:49 -0300

    test: garante Login mostra valido se email valida

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -55,6 +55,15 @@ describe('Login component', () => {
     expect(passwordStatus.textContent).toBe('🔴')
   })
 
+  test('should show valid email state if Validation succeds', () => {
+    const { sut, valitationStub } = makeSut()
+    valitationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Tudo certo')
+  })
+
   test('should show valid password state if Validation succeds', () => {
     const { sut, valitationStub } = makeSut()
     valitationStub.errorMessage = null
