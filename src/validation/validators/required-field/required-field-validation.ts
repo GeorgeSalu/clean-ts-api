d118ca9afb7f04c3bbbc3f384409451c606bc370
George Salu <george.salu10@gmail.com>
2021-02-12 13:25:05 -0300

    refactor: rename variable

--- src/validation/validators/required-field/required-field-validation.ts
+++ src/validation/validators/required-field/required-field-validation.ts
@@ -3,7 +3,6 @@ import { RequiredFieldError } from '@/validation/errors'
 
 export class RequiredFieldValidation implements FieldValidation {
   constructor (readonly field: string) {}
-  fieldName: string
 
   validate (value: string): Error {
     return value ? null : new RequiredFieldError()
