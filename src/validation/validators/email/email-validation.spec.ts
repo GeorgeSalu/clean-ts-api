170a3761a3a2811b77c9a9e619ffe3aaf63ec465
George Salu <george.salu10@gmail.com>
2021-02-12 11:16:41 -0300

    refactor: use faker

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -4,13 +4,13 @@ import faker from 'faker'
 
 describe('EmailValidation', () => {
   test('should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
+    const sut = new EmailValidation(faker.random.word())
     const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('should return falsy if email is valid', () => {
-    const sut = new EmailValidation('email')
+    const sut = new EmailValidation(faker.random.word())
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
