c799ef5ef1c80788c57c979ba73abf2c5d83986e
George Salu <george.salu10@gmail.com>
2021-02-19 10:21:07 -0300

    refactor: organize imports

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,9 +3,8 @@ import Styles from './login-styles.scss'
 import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/from/form-context'
 import { Validation } from '@/presentation/protocols/validation'
-import { Authentication } from '@/domain/usecases'
+import { Authentication, SaveAccessToken } from '@/domain/usecases'
 import { Link, useHistory } from 'react-router-dom'
-import { SaveAccessToken } from '@/domain/usecases/save-access-token'
 
 type Props = {
   validation: Validation
