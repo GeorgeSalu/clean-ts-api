a3d6f72dc2deb117badc8ab42103f9e2d4e4d686
George Salu <george.salu10@gmail.com>
2021-03-10 13:15:49 -0300

    refactor: change usecases to use internal typealias

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,6 +1,5 @@
 import { SurveyList } from '@/presentation/pages'
 import { LoadSurveyList } from '@/domain/usecases'
-import { SurveyModel } from '@/domain/models'
 import { mockSurveyListModel } from '@/domain/test'
 import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
@@ -10,7 +9,7 @@ class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0
   surveys = mockSurveyListModel()
 
-  async loadAll (): Promise<SurveyModel[]> {
+  async loadAll (): Promise<LoadSurveyList.Model[]> {
     this.callsCount++
     return this.surveys
   }
