3dfbccb128bfaf47f807902e575a2c8452edbd30
George Salu <george.salu10@gmail.com>
2021-03-14 21:42:00 -0300

    refactor: move error to its own component

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,6 +1,6 @@
 import Styles from './survey-list-styles.scss'
-import { Header, Footer } from '@/presentation/components'
-import { SurveyContext, SurveyListItem, Error } from '@/presentation/pages/survey-list/components'
+import { Header, Footer, Error } from '@/presentation/components'
+import { SurveyContext, SurveyListItem } from '@/presentation/pages/survey-list/components'
 import { useErrorHandler } from '@/presentation/hooks'
 import { LoadSurveyList } from '@/domain/usecases'
 import React, { useEffect, useState } from 'react'
@@ -11,17 +11,18 @@ type Props = {
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
   const handleError = useErrorHandler((error: Error) => {
-    setState({ ...state, error: error.message })
+    setState(old => ({ ...old, error: error.message }))
   })
   const [state, setState] = useState({
     surveys: [] as LoadSurveyList.Model[],
     error: '',
     reload: false
   })
+  const reload = (): void => setState(old => ({ surveys: [], error: '', reload: !old.reload }))
 
   useEffect(() => {
     loadSurveyList.loadAll()
-      .then(surveys => setState({ ...state, surveys }))
+      .then(surveys => setState(old => ({ ...old, surveys })))
       .catch(handleError)
   }, [state.reload])
 
@@ -31,7 +32,10 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <SurveyContext.Provider value={{ state, setState }}>
-          {state.error ? <Error /> : <SurveyListItem />}
+          {state.error
+            ? <Error error={state.error} reload={reload} />
+            : <SurveyListItem />
+          }
         </SurveyContext.Provider>
       </div>
       <Footer />
